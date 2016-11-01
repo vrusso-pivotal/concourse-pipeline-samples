7929305dedf463dfbac4362407314ce6895e7916
azwickey-pivotal <vania12R>
2016-11-01 13:18:08 -0400

    testing demo

--- boot-sample/ci/tasks/package.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -xe
-
-cd git-assets/boot-sample
-mvn package
-cp target/concourse-demo-*.jar ../../app-output/concourse-demo.jar
--- boot-sample/ci/tasks/test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -xe
-
-cd git-assets/boot-sample
-mvn test
